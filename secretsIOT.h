9af0951e880b17dc76a87310e0fdc423440e1c5a
guyguy_ubuntutu <guydvir.tech@gmail.com>
2026-07-31 19:22:27 +0300

    update libs

---
+++ secretsIOT.h
@@ -0,0 +1,26 @@
+#define SSID_ID "dvirzIoT"
+#define PASS_WIFI "GdSd13100301"
+#define MQTT_USER "guy"
+#define MQTT_PASS "kupelu9e"
+#define MQTT_SERVER1 "192.168.30.2"
+
+#define tok "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"
+
+#define CHAT_ID "596123373"
+#define BOT_TOKEN_2 "497268459:AAESYm27tJfNXwnnnn0slbmWnkqvbWgQEyw" //homepi_bot
+#define BOT_TOKEN "812406965:AAEaV-ONCIru8ePuisuMfm0ECygsm5adZHs" //botbot
+#define BOT_TOKEN_3 "1238925698:AAGWARuQ2eyx2i0ui2sCp_mM7xInTsKgUuM" //guyd_test2bot
+
+
+#define GMAIL_USER "guydvir.tech@gmail.com"
+#define GMAIL_P "GdSd13100301"
+
+#define IFTTT_KEY "cFLymB4JT9tlODsKLFn9TA"
+#define apiKey_whatapss "4060945"
+
+#define NONE 1
+
+#define MAX_ESP8266_HEAP 52472
+#define MAX_ESP32_HEAP 343976
+
+
